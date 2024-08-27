f09f0c7fdc3aa0dcfcf7686c2fcde18f1673e47e
rto <hndoss@gmail.com>
2024-08-27 14:48:05 -0600

    feat(vue): Initialize vue project

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <navigation-menu></navigation-menu>
+
+    </div>
+</template>
+
+<script>
+import NavigationMenu from '../components/NavigationMenu.vue';
+export default {
+    name: 'Home',
+    components: {
+        NavigationMenu
+    }
+}
+</script>
+
+<style></style>
