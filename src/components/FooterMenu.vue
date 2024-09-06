2d57ca0212f067681857a782318bc6d811336bec
rto <hndoss@gmail.com>
2024-09-06 09:13:48 -0600

    fix(footer): Add FooterMenu component

---
+++ src/components/FooterMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <footer
+        class="flex relative gap-10 items-center px-7 mt-44 min-h-[193px] max-md:px-5 max-md:mt-10 max-md:max-w-full">
+
+        <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/8f02c129669cdff87917f5edbaeb38e16b2485e76d36fe627761acee18113fcb?placeholderIfAbsent=true&apiKey=0ec1ed00d9eb4adfb001d83c3aaae589"
+            alt="" class="object-contain z-0 shrink-0 self-stretch my-auto aspect-[1.53] min-w-[240px] w-[286px]" />
+
+
+        <nav class="flex z-0 flex-col self-stretch my-auto">
+            <ul class="flex flex-col w-full text-2xl text-teal-400 list-none p-0">
+                <li class="text-right"><a href="#portfolio" class="no-underline text-teal-400">Portafolio</a></li>
+                <li class="mt-2 text-right"><a href="#contact" class="no-underline text-teal-400">Contáctanos</a></li>
+                <li class="mt-2"><a href="#about" class="no-underline text-teal-400">Acerca de Nosotros</a></li>
+            </ul>
+
+            <div class="flex gap-4 items-center self-end mt-4">
+                <a href="#" aria-label="Social media link">
+                    <img loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/a588b2e5269fdb564da070ad9a64ffc20cc0a146c3598ac689044549e68cef02?placeholderIfAbsent=true&apiKey=0ec1ed00d9eb4adfb001d83c3aaae589"
+                        alt="" class="object-contain shrink-0 self-stretch my-auto w-8 aspect-square" />
+                </a>
+                <a href="#" aria-label="Social media link">
+                    <img loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/e152466ef40bbee522973b9897bd04c44dc8f0d907ed00a19caf3d5ae71f5f86?placeholderIfAbsent=true&apiKey=0ec1ed00d9eb4adfb001d83c3aaae589"
+                        alt="" class="object-contain shrink-0 self-stretch my-auto w-8 bg-blend-normal aspect-square" />
+                </a>
+            </div>
+        </nav>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="1441" height="193" viewBox="0 0 1441 193" fill="none">
+            <path d="M1441 0H0V193H1441V0Z" fill="url(#paint0_linear_47_13)" />
+            <defs>
+                <linearGradient id="paint0_linear_47_13" x1="0" y1="96.5005" x2="1441" y2="96.5005"
+                    gradientUnits="userSpaceOnUse">
+                    <stop offset="8.15152e-07" stop-color="#072F2E" />
+                    <stop offset="1" stop-color="#090901" />
+                </linearGradient>
+            </defs>
+        </svg>
+    </footer>
+</template>
