0cd475d255ff5854cbfc75384f4ab61b7f35a0e0
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-15 16:30:38 -0600

    [AR-2] Fix(Tailwind): customize box shadow.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,10 @@ export default {
         gray: '#717171',
         snowGray: "#F0F0F3",
         lightGreen:"#00FFAA"
-      }
+      },
+      boxShadow: {
+        '3xl': '-10px -10px 30px 0px #FFF, 10px 10px 30px 0px rgba(174, 174, 192, 0.40);',
+      },
     },
   },
   plugins: [
