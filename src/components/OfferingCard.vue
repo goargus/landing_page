86fb2d49748525fa667378a37e62db62d7659922
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-15 16:28:14 -0600

    [AR-2] Fix(Index): make the cards have and the same size.

--- src/components/OfferingCard.vue
+++ src/components/OfferingCard.vue
@@ -1,11 +1,12 @@
 <template>
     <article
-        class="flex flex-col grow shrink justify-center items-center self-stretch px-5 py-9 my-auto bg-gray-100 rounded-2xl shadow-lg min-h-[353px] min-w-[240px] w-[282px]">
+        class="flex flex-col justify-center items-center bg-gray-100 rounded-2xl shadow-3xl h-[350px] w-[350px] px-5 py-9">
         <img :src="imageSrc" :alt="imageAlt" loading="lazy" class="object-contain w-[88px]" />
-        <h3 class="mt-8 text-2xl font-bold">{{ title }}</h3>
-        <p class="mt-8 text-lg">{{ description }}</p>
+        <h3 class="mt-6 text-2xl font-bold text-center">{{ title }}</h3>
+        <p class="mt-6 text-lg text-center">{{ description }}</p>
     </article>
 </template>
+
 
 <script lang="ts">
 
