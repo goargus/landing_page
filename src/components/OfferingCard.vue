c0ead2989b1c4a59c7a9dd67b41ed4a521fa4e95
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-16 09:54:13 -0600

    [AR-2] Fix(index): alignment of texts and images.

--- src/components/OfferingCard.vue
+++ src/components/OfferingCard.vue
@@ -1,11 +1,12 @@
 <template>
-    <article
-        class="flex flex-col justify-center items-center bg-gray-100 rounded-2xl shadow-3xl h-[350px] w-[350px] px-5 py-9">
-        <img :src="imageSrc" :alt="imageAlt" loading="lazy" class="object-contain w-[88px]" />
-        <h3 class="mt-6 text-2xl font-bold text-center">{{ title }}</h3>
-        <p class="mt-6 text-lg text-center">{{ description }}</p>
-    </article>
-</template>
+    <div class="flex flex-col justify-between bg-gray-100 rounded-2xl shadow-3xl h-[400px] w-[350px] px-5 py-9">
+      <div class="flex justify-center">
+        <img :src="imageSrc" :alt="imageAlt" class="object-contain h-24" />
+      </div>
+      <h3 class="mt-6 text-2xl font-bold text-center min-h-[60px] flex items-center justify-center">{{ title }}</h3>
+      <p class="mt-6 text-lg text-center min-h-[80px] flex place-itmes-start justify-center">{{ description }}</p>
+    </div>
+  </template>
 
 
 <script lang="ts">
