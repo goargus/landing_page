545dfdab0079a080f4a03e66ec59cafe20822a20
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-16 15:43:04 -0600

    [AR-2] Fix(Footer): Add margin top in the footer.

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -7,7 +7,7 @@
       alt="" class="object-contain z-0 shrink-0 self-stretch my-auto aspect-[1.53] min-w-[240px] w-[286px]" />
 
 
-      <nav class="flex z-0 flex-col self-stretch my-auto">
+      <nav class="flex z-0 flex-col self-stretch my-auto mt-4">
   <ul class="flex flex-col w-full text-2xl text-teal-400 list-none p-0">
     <li class="text-right">
       <router-link to="/portfolio" class="no-underline text-lightGreen-400">Portafolio</router-link>
