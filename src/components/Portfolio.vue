743e1b14af4bf4217cd465f0282168a4a7f82681
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-22 11:15:32 -0600

    [AR-2] Fix(Index): import the image example for assets and use.

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -11,6 +11,7 @@
 <script lang="ts">
 import { DefineComponent } from 'vue';
 import Carousel from './carousel.vue';
+import mockup from '../assets/mockup.png';
 
 export default {
   name: 'Portfolio',
@@ -27,17 +28,17 @@ export default {
     return {
       proyects: [
         {
-          imagesrc: 'https://s3-alpha-sig.figma.com/img/9df2/e43a/a33d73a7bd56af1a9509f7d0965b72d6?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KT6CC6xZZ5r093lXqeBKzznGQ9NUfVjn33oDZ4bnOMXy2n1U951xI1q7p2gr1RY0bEyMTYmvaFprvdpXGEAdsADPKqqIyz453P6bsMiwxg7bq26ZeGV249ID1JSpCRKZ5X1dAcmiTXdymt80Ug3qrG0qJ1fANOoom0kVTuAfBCXuzarQ3bTc5v4xEXo13ompRtzfft3a3a8wdvadKIySBXleG4yd3eSuMjIBhcWlbOC9XfQpmg82XYl-URNnX7fFTtkChA0zIjaIG80KO4YQeEaMNMUsxtHNkeuCWWsGYQSUnzQVCnvz8uxFihiEO5tlRi1odNFsg88vcz5qPYUPcw__',
+          imagesrc: mockup ,
           imageAlt: 'Descripcion',
           link: '#',
         },
         {
-        imagesrc: 'https://s3-alpha-sig.figma.com/img/9df2/e43a/a33d73a7bd56af1a9509f7d0965b72d6?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KT6CC6xZZ5r093lXqeBKzznGQ9NUfVjn33oDZ4bnOMXy2n1U951xI1q7p2gr1RY0bEyMTYmvaFprvdpXGEAdsADPKqqIyz453P6bsMiwxg7bq26ZeGV249ID1JSpCRKZ5X1dAcmiTXdymt80Ug3qrG0qJ1fANOoom0kVTuAfBCXuzarQ3bTc5v4xEXo13ompRtzfft3a3a8wdvadKIySBXleG4yd3eSuMjIBhcWlbOC9XfQpmg82XYl-URNnX7fFTtkChA0zIjaIG80KO4YQeEaMNMUsxtHNkeuCWWsGYQSUnzQVCnvz8uxFihiEO5tlRi1odNFsg88vcz5qPYUPcw__',
+        imagesrc: mockup,
           imageAlt: 'Descripcion',
           link: '#',
         },
         {
-        imagesrc: 'https://s3-alpha-sig.figma.com/img/9df2/e43a/a33d73a7bd56af1a9509f7d0965b72d6?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KT6CC6xZZ5r093lXqeBKzznGQ9NUfVjn33oDZ4bnOMXy2n1U951xI1q7p2gr1RY0bEyMTYmvaFprvdpXGEAdsADPKqqIyz453P6bsMiwxg7bq26ZeGV249ID1JSpCRKZ5X1dAcmiTXdymt80Ug3qrG0qJ1fANOoom0kVTuAfBCXuzarQ3bTc5v4xEXo13ompRtzfft3a3a8wdvadKIySBXleG4yd3eSuMjIBhcWlbOC9XfQpmg82XYl-URNnX7fFTtkChA0zIjaIG80KO4YQeEaMNMUsxtHNkeuCWWsGYQSUnzQVCnvz8uxFihiEO5tlRi1odNFsg88vcz5qPYUPcw__',
+        imagesrc: mockup,
           imageAlt: 'Descripcion',
           link: '#',
         },
