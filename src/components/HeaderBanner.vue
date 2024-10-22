6e8638f0c4da2753b2c4c4504838723a646131f2
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-22 15:06:20 -0600

    [AR-2] Fix(Index): Remove the style of the class.

--- src/components/HeaderBanner.vue
+++ src/components/HeaderBanner.vue
@@ -1,13 +1,23 @@
 <template>
     <header class="relative">
         <img src="../assets/Bannerpicture.png" alt="Banner" class="object-cover w-full h-full" />
-        <div class="absolute inset-0" style="background: linear-gradient(90deg, rgba(7, 47, 46, 0.8) 40%, rgba(9, 9, 1, 0.8) 100%);">
+        <div class="degraded">
         </div>
 
 <div class="absolute inset-0 flex items-center justify-center">
     <img loading="lazy"
       src="../assets/defaultLogo.png"
-      alt="" class="object-contain z-0 shrink-0 self-stretch my-auto aspect-[1.53] min-w-[483px] w-[316px]" />
+      alt="" class="bg-image" />
 </div>
        </header>
 </template>
+
+<style>
+.degraded{
+    @apply absolute inset-0;
+    background: linear-gradient(90deg, rgba(7, 47, 46, 0.8) 40%, rgba(9, 9, 1, 0.8) 100%);
+}
+.bg-image{
+    @apply object-contain z-0 shrink-0 self-stretch my-auto aspect-[1.53] min-w-[483px] w-[316px];
+}
+</style>
