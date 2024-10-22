2fa37c83ba58bd9bb8e19eb702ee22249a066c76
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-22 16:42:49 -0600

    [AR-2] Fix(Index): change pryects by project and use class in style.

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,43 +1,40 @@
 <template>
   <div class="relative mt-4 overflow-hidden h-[680px]">
     <div
-      class="flex transition-transform duration-300" :style="`transform: translateX(-${currentIndex * 100}%)`">
+      class="flex transition" :style="`transform: translateX(-${currentIndex * 100}%)`">
       <div
-        v-for="(proyects, index) in proyects"
+        v-for="(project, index) in projects"
         :key="index"
         class="flex-none w-full flex flex-col justify-center items-center relative">
-        
-        <div class="absolute bg-gray-500 rounded-[30px] shadow-3xl border-2 border-white z-0"
+        <div class="bg-square"
              style="width: 60%; height: 100%; top: 5%;">
         </div>
-
-        <img :src="proyects.imagesrc" :alt="proyects.imageAlt"
-          class="relative z-10 object-contain max-w-full rounded-[30px]"/>
-
-        <a :href="proyects.link"
-          class="mt-4 z-20 inline-block bg-gray-300 text-gray-700 px-20 py-2.5 transition shadow-3xl rounded-3xl border-solid border-2 border-white"
+        <img :src="project.imagesrc" :alt="project.imageAlt"
+          class="image-size"/>
+        <a :href="project.link"
+          class="transition button-website"
           target="_blank">Ver Website</a>
       </div>
     </div>
     <button
-      @click="prevSlide" class="absolute top-1/2 left-20 transform -translate-y-1/2 rounded-full p-2">
+      @click="prevSlide" class="transform arrow-left">
       <img src="../assets/arrow.svg" alt="Arrow left" class="w-139 h-189" />
     </button>
     <button
-     @click="nextSlide"class="absolute top-1/2 right-20 transform -translate-y-1/2 rounded-full p-2">
+     @click="nextSlide"class="transform arrow-right">
       <img src="../assets/arrow-2.svg" alt="Arrow right" class="w-139 h-189" />
     </button>
   </div>
 </template>
 
 <script lang="ts">
-import {proyects} from '../types/caroItems'
+import {projects} from '../types/caroItems'
 
 export default {
   name: 'Carousel',
   props: {
-    proyects: {
-      type: Array as () => proyects[],
+    projects: {
+      type: Array as () => projects[],
       required: true,
     },
   },
@@ -48,11 +45,32 @@ export default {
   },
   methods: {
     nextSlide() {
-      this.currentIndex = (this.currentIndex + 1) % this.proyects.length;
+      this.currentIndex = (this.currentIndex + 1) % this.projects.length;
     },
     prevSlide() {
-      this.currentIndex =(this.currentIndex - 1 + this.proyects.length) % this.proyects.length;
+      this.currentIndex =(this.currentIndex - 1 + this.projects.length) % this.projects.length;
     },
   },
 };
 </script>
+
+<style>
+.transition{
+  @apply transition-transform duration-300;
+};
+.bg-square{
+  @apply absolute rounded-[30px] shadow-3xl border-2 border-white z-0;
+};
+.image-size{
+@apply relative z-10 object-contain max-w-full rounded-[30px];
+};
+.button-website{
+@apply mt-4 z-20 inline-block  px-20 py-2.5 shadow-3xl rounded-3xl border-solid border-2 border-white;
+};
+.arrow-left{
+@apply absolute top-1/2 left-20  -translate-y-1/2 rounded-full p-2;
+}
+.arrow-right {
+  @apply absolute top-1/2 right-20 -translate-y-1/2 rounded-full p-2
+}
+</style>
