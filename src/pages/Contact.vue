c6ef5babba840fe4d96fa05ed5e9b9a45b56438d
JMB⚡️ <temporal2@MacBook-Pro.local>
2024-10-28 11:45:58 -0600

    [AR-3] Fix(Contact): Add the paragraph.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,8 +1,17 @@
 <template>
-     <div class="w-full">
-        <header-banner title="Cotáctanos"></header-banner>
-    </div>
+<div class="w-full">
+<header-banner title="Contáctanos"></header-banner>
+<p class="font-Inter">¡Te invitamos a ponerte en contacto con Go Argus! Estamos aquí para ayudarte a llevar tu presencia digital al siguiente nivel. Si tienes un proyecto en mente o simplemente quieres saber más sobre nuestros servicios, no dudes en escribirnos. Estaremos encantados de trabajar contigo y encontrar la mejor solución para tus necesidades. ¡Hablemos y comencemos a crear algo increíble juntos!
+</p>
+</div>
 </template>
+
 <script setup lang="ts">
 import HeaderBanner from '../components/HeaderBanner.vue';
 </script>
+
+<style>
+p{
+    @apply text-gray text-center  text-[18px] font-normal leading-normal w-[1156px] h-[130px] flex-shrink-0 mx-auto p-20;
+}
+</style>
