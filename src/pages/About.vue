9a30941640151e81a811cd525f20f8156fa8a19f
JMB⚡️ <temporal2@MacBook-Pro.local>
2025-03-05 16:26:57 -0600

    [AR-4] Fix(About Us): Add the header banner in the page.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,8 @@
 <template>
-    About Us
+<div class="w-full">
+    <header-banner title="Acerca de nosotros"></header-banner>
+</div>
 </template>
+<script lang="ts" setup>
+import HeaderBanner from '../components/HeaderBanner.vue';
+</script>
