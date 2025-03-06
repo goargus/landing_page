88636030b0a0872fb2b861caced82755daaaf51b
JMB⚡️ <temporal2@MacBook-Pro.local>
2025-03-05 19:03:26 -0600

    [AR-4] Fix(About Us): Add the h1 title and the paragraph, The style was also configured.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,25 @@
 <div class="w-full">
     <header-banner title="Acerca de nosotros"></header-banner>
 </div>
+<div class="p-20">
+<h1 class="h1title">Unimos Estética & Funcionalidad</h1>
+<p class="font-Inter paragraphs">
+    Nos especializamos en ofrecer soluciones digitales integrales
+    que impulsan el éxito de nuestros clientes en el entorno online.
+    Desde el diseño web profesional, funcional y responsivo hasta el alojamiento de dominios. 
+    Nos comprometemos a proporcionar servicios de alta calidad que garantizan una presencia web efectiva y duradera.
+    Nuestro enfoque está en entender las necesidades únicas de cada cliente y ofrecerles las herramientas para destacar en el mundo digital.
+    </p>
+</div>
 </template>
 <script lang="ts" setup>
 import HeaderBanner from '../components/HeaderBanner.vue';
 </script>
+<style>
+.h1title{
+    @apply text-3xl font-semibold text-center text-txtcolor mb-5;
+}
+.paragraphs {
+  @apply text-txtcolor text-center text-[18px] font-normal leading-normal w-[950px] h-[130px] flex-shrink-0 mx-auto p-10;
+}
+</style>
