2615548a21175807205a723fcf22d88f32b1f344
JMB⚡️ <temporal2@MacBook-Pro.local>
2025-03-11 12:18:29 -0600

    [AR-4] Fix(Contact): Remove the style and adjustment of the paragraph.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <header-banner title="Contáctanos"></header-banner>
-    <p class="font-Inter pcontact">
+    <p class="font-Inter w-[1370px] h-[130px] p-20 text-center">
       ¡Te invitamos a ponerte en contacto con Go Argus! Estamos aquí para ayudarte a llevar tu presencia digital al
       siguiente nivel. Si tienes un proyecto en mente o simplemente quieres saber más sobre nuestros servicios, no dudes
       en escribirnos. Estaremos encantados de trabajar contigo y encontrar la mejor solución para tus necesidades.
@@ -18,8 +18,3 @@ import HeaderBanner from '../components/HeaderBanner.vue';
 import ContactForm from '../components/ContactForm.vue';
 </script>
 
-<style>
-.pcontact {
-  @apply text-txtcolor text-center text-[18px] font-normal leading-normal w-[1500px] h-[130px] flex-shrink-0 mx-auto p-20;
-}
-</style>
