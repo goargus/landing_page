e935fb7960377d72a236226350d2e5f5338960f2
JMB⚡️ <temporal2@MacBook-Pro.local>
2025-03-11 12:13:14 -0600

    [AR-4] Fix(About Us): Ajust the code for more easy read and remove the  style.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,34 +1,31 @@
 <template>
-<div class="w-full">
-    <header-banner title="Acerca de nosotros"></header-banner>
-</div>
-<div class="p-20">
-<h1 class="h1title">Unimos Estética & Funcionalidad</h1>
-<p class="font-Inter paragraphs">
-    Nos especializamos en ofrecer soluciones digitales integrales
-    que impulsan el éxito de nuestros clientes en el entorno online.
-    Desde el diseño web profesional, funcional y responsivo hasta el alojamiento de dominios. 
-    Nos comprometemos a proporcionar servicios de alta calidad que garantizan una presencia web efectiva y duradera.
-    Nuestro enfoque está en entender las necesidades únicas de cada cliente y ofrecerles las herramientas para destacar en el mundo digital.
-    </p>
-</div>
-<div class="flex p-20 mb-20 justify-center items-center">
-                        <icon-section> </icon-section>
-                    </div>
-<div>
-    <quotebanner></quotebanner>
-</div>
-</template>
-<script lang="ts" setup>
-import HeaderBanner from '../components/HeaderBanner.vue';
-import IconSection from '../components/IconSection.vue';
-import quotebanner from '../components/quotebanner.vue';
-</script>
-<style>
-.h1title{
-    @apply text-3xl font-semibold text-center text-txtcolor mb-5;
-}
-.paragraphs {
-  @apply text-txtcolor text-center text-[18px] font-normal leading-normal w-[950px] h-[130px] flex-shrink-0 mx-auto p-10;
-}
-</style>
+    <div class="w-full">
+      <HeaderBanner title="Acerca de nosotros" />
+    </div>
+  
+    <div class="p-20 text-center">
+      <h1 class="mb-5">Unimos Estética & Funcionalidad</h1>
+      <p class="p-5 max-w-[1000px]">
+        Nos especializamos en ofrecer soluciones digitales integrales que impulsan el éxito de nuestros clientes en el entorno online.
+        Desde el diseño web profesional, funcional y responsivo hasta el alojamiento de dominios.
+        Nos comprometemos a proporcionar servicios de alta calidad que garantizan una presencia web efectiva y duradera.
+        Nuestro enfoque está en entender las necesidades únicas de cada cliente y ofrecerles las herramientas para destacar en el mundo digital.
+      </p>
+    </div>
+  
+    <div class="flex p-1 mb-20 justify-center items-center">
+      <IconSection />
+    </div>
+  
+    <div>
+      <QuoteBanner />
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import HeaderBanner from "../components/HeaderBanner.vue";
+  import IconSection from "../components/IconSection.vue";
+  import QuoteBanner from "../components/QuoteBanner.vue";
+  </script>
+
+  
