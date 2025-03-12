aca96e318fc1081fd04704fd7bc9eccf28dad6df
JMB⚡️ <temporal2@MacBook-Pro.local>
2025-03-12 15:36:17 -0600

    [AR-4] Fix(General): Use camelCase.

--- src/components/IconSection.vue
+++ src/components/IconSection.vue
@@ -15,8 +15,8 @@
 
 <script lang="ts">
 import IconCard from '../components/IconCard.vue';
-import WebDesign from '../assets/webdesign2.svg';
-import LaptopIcon from '../assets/LaptopIcon.svg';
+import webDesign from '../assets/webDesign2.svg';
+import laptopIcon from '../assets/laptopIcon.svg';
 
 export default {
     name: 'IconSection',
@@ -29,13 +29,13 @@ export default {
         return {
             offerings: [
                 {
-                    imageSrc: WebDesign,
+                    imageSrc: webDesign,
                     imageAlt: 'Desarrollo',
                     title: 'Desarrollo',
                     description: 'Contamos con un equipo de desarrolladores expertos dedicados a crear soluciones digitales de alta calidad. Nuestros especialistas en desarrollo están listos para transformar ideas en plataformas funcionales y personalizadas, adaptándose a las necesidades de cada proyecto. Desde el diseño hasta la implementación, trabajamos con las mejores prácticas para ofrecer resultados efectivos y escalables.',
                 },
                 {
-                    imageSrc: LaptopIcon,
+                    imageSrc: laptopIcon,
                     imageAlt: 'Diseño',
                     title: 'Diseño',
                     description: 'Nuestro equipo de expertos en UX/UI se especializa en crear experiencias intuitivas y atractivas. Nos enfocamos en el diseño centrado en el usuario, asegurando que cada interfaz no solo sea visualmente impactante, sino también fácil de usar. Con un enfoque en funcionalidad y estética, optimizamos cada proyecto para ofrecer una experiencia memorable y efectiva para el usuario.',
