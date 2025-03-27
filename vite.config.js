193fa5d7797501ace11389fa4a7dd321b046b817
JMB-7 <141590033+JMB-7@users.noreply.github.com>
2025-03-27 14:22:10 -0600

    Problem deploy (#32)
    * Fix(Deploy): Separate build and deploy jobs in GitHub Actions workflow.
    
    * Fix(Deploy):Update Vite configuration: set base path, specify output directory, and ensure proper minification settings.
    
    ---------
    
    Co-authored-by: Johny Mejia <jhonnymejia100@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages';
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",  
   plugins: [
     vue(),
     Pages(),
   ],
   build: {
-    minify: 'terser',
+    outDir: "dist",  
+    minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
+        drop_debugger: true,
+      },
     },
     rollupOptions: {
       output: {
         manualChunks: {
-          'vendor': ['vue', 'vue-router']
-        }
-      }
-    }
-  }
+          vendor: ["vue", "vue-router"],
+        },
+      },
+    },
+  },
 })
