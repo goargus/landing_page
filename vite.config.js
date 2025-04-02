8d17b78fd8b73eda02f996b92c620215eedc67b4
Johny Mejia <jhonnymejia100@gmail.com>
2025-04-01 18:40:31 -0600

    Feat: Add vite-plugin-obfuscator for code obfuscation in Vite configuration. Update package.json and package-lock.json to include the new dependency.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import { viteObfuscateFile } from 'vite-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
   plugins: [
     vue(),
     Pages(),
+    viteObfuscateFile({
+      global: true, 
+      stringArray: true, 
+      transformObjectKeys: true, 
+    }),
   ],
   build: {
     outDir: "dist",  
